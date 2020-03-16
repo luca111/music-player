dd230231ddebc1cc6ff4353353e69de2bbe07c85
Luca Comito <luca.comito@gmail.com>
2020-03-16 19:04:38 +0100

    change relative path

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_RELATIVE_PATH =
   // eslint-disable-next-line no-undef
-  process.env === "production" ? "/.netlify/functions" : "";
+  process.env.NETLIFY ? "/.netlify/functions" : "";
